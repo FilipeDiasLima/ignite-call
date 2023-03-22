b84fbdb4e0bf4eb755721ee8e8a307f17a6ee673
filipediaslima <filipdiasdelima@gmail.com>
2023-03-21 23:25:27 -0300

    monte de coisa

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,67 @@
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { ArrowRight, Check } from "phosphor-react";
+
+import { Container, Header } from "../styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
+
+export default function ConnectCalendar() {
+  const router = useRouter();
+  const session = useSession();
+  console.log("🚀 ~ file: index.page.tsx:12 ~ Register ~ session:", session);
+
+  const hasAuthError = !!router.query.error;
+  const isSignedIn = session.status === "authenticated";
+
+  async function handleConnectCalendar() {
+    await signIn("google");
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+
+        {hasAuthError && (
+          <AuthError>
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  );
+}
